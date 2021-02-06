78029331c2a819bb31b5e6aa1ac4bae8fac8a5d6
Mahmudul Hasan <mahmudulh883@gmail.com>
2021-02-06 23:52:58 +0600

    truthy and falsy

---
+++ true-false.js
@@ -0,0 +1,26 @@
+let a=[false]
+if (a) {
+   console.log('true');
+} else {
+   console.log('false');
+}
+
+/*
+truthy
+1.any number without zero
+2.any string
+3.'gap'
+4.[] or any value in it
+5.{} or any value in it
+6.'false'
+
+falsy
+
+1.zero
+2.empty string
+3.undefined
+4.null
+5.NaN
+6.false
+
+*/
