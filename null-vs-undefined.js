ac92c1fd58807c4f897f4347b2001309b85624dc
Mahmudul Hasan <mahmudulh883@gmail.com>
2021-02-07 00:13:12 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,31 @@
+// 1.undefined
+//let pakhi;
+//console.log(pakhi);
+
+function add(num1,num2) {
+   const add= num1 + num2;
+   // return add                         it show undefined cause not return anything
+}
+const result =add(5,5)
+console.log(result);
+
+
+
+// 2 undefined
+
+const person={name:'hasan',phone:'165',gf:'adiba'}
+console.log(person.age); // age are not a property of person
+
+const array=[2,3,4,5]
+console.log(array[11]); // 11 th element are not present
+
+const value=undefined; //value set undefined
+console.log(value);
+
+//null explicitly set korbo 
+// undefined falsy
+//not existence
+//undefined can findout but null are not defined it explicitly set
+
+
+
